6255fb51e8f463463126e469a02b515f0c483cf6
chai555-pixel <chaithrak636016@gmail.com>
2024-11-12 11:48:57 +0530

    new status

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,29 @@
 
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html')
+
