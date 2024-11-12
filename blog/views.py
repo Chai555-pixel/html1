7c53fa8d33fe0947478ba0f4b2c261a8e59bf203
chai555-pixel <chaithrak636016@gmail.com>
2024-11-12 09:42:45 +0530

    html file

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+# blog/views.py
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
